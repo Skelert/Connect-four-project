c4583c0b43a371142468455521f3ea96584a1982
Skelert <skelert715@gmail.com>
2025-03-08 20:48:37 -0700

    Update info.php

--- info.php
+++ info.php
@@ -1,19 +1,29 @@
-<?php // index.php
-$your_Board= new info();
-$your_Board->constructor(7,6, array("Smart","Random"));
+<?php
+// index.php 
 
-class info{
-
+ 
+class info {
+  
     var $width;
     var $height;
     var $strategies;
 
-    function constructor($WIDTH, $HEIGHT, $STRATRGIES){
-        $this->width= $WIDTH;
-        $this->height= $HEIGHT;
-        $this->strategies= $STRATRGIES;
+    
+     //Constructor for the info class
+    
+    function constructor($WIDTH, $HEIGHT, $STRATEGIES) {
+        $this->width = $WIDTH;
+        $this->height = $HEIGHT;
+        $this->strategies = $STRATEGIES;
     }
 }
 
+// Create instance of the game board 
+$your_Board = new info();
+
+// Initialize the game board with dimensions 7x6 and AI strategies
+$your_Board->constructor(7, 6, array("Smart", "Random"));
+
+// Output the game board configuration as JSON
 echo json_encode($your_Board);
 ?>
